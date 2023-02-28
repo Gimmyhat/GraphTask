c63a52025a2c1e40b993b1bfa994e965f6217eaa
Gimmyhat <nikitchenko.as@gmail.com>
2023-02-28 18:11:33 +0800

    Drop files from .gitignore

--- start_neo4j.bat
+++
@@ -1,7 +0,0 @@
-docker run ^
---name my-neo4j-container ^
--p 7474:7474 -p 7687:7687 ^
--v C:\Users\Sasha\neo4j\data:/data ^
--v C:\Users\Sasha\neo4j\logs:/logs ^
--e NEO4J_AUTH=neo4j/0123456789 ^
--d neo4j
